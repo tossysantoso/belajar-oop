a4f80ac91d4bcb6d8b78fd394a1d2deaa5d02517
Tossy Santoso <tossysantos2@gmail.com>
2020-07-30 20:35:23 +0700

    index penampil animal

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+require_once 'animal.php';
+require_once 'ape.php';
+require_once 'frog.php';
+
+$sheep = new Animal("shaun");
+
+echo $sheep->name;
+echo "<br>"; // "shaun"
+$sheep->set_legs(2);
+echo $sheep->get_legs(); // 2
+echo $sheep->cold_blooded; // false
+
+// NB: Boleh juga menggunakan method get (get_name(), get_legs(), get_cold_blooded())
+
+//ape
+$sungokong = new Ape("kera sakti");
+echo"<br><br>";
+echo $sungokong->yell(); // "Auooo"
+//echo"<br>";
+//$sungokong->set_legs(2);
+//echo $sungokong->get_legs();
+
+//frog
+$kodok = new Frog("buduk");
+echo"<br><br>";
+echo $kodok->jump() ; // "hop hop"
+?>
