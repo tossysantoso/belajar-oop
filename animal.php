b68da9cd4fe95db6508ee0268e7c07d7651b7170
Tossy Santoso <tossysantos2@gmail.com>
2020-07-30 20:36:01 +0700

    create class animal

---
+++ animal.php
@@ -0,0 +1,28 @@
+<?php
+
+
+//class animal
+class Animal{
+    //properti
+    public $legs;
+    public $cold_blooded = false;
+    public $name;
+    
+    //methode construct
+    public function __construct($name)
+    {
+        $this->name = $name;
+    }
+
+    //methode set
+    public function set_legs($legs){
+        $this->legs = $legs;
+    }
+
+    //methode get
+    public function get_legs(){
+        return $this->legs;
+    }
+}
+
+?>
